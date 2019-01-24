a0bb12b79ba7cf58b1305022d8c9be92d624396b
knickheal75 <blitzkrieg0705@gmail.com>
2019-01-24 14:39:24 +0530

    Added JS Folder

---
+++ js/js1.js
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<script type = "text/javascript">
+function fun(n)
+{    
+	if (n == 4)       	
+		return n;    
+	else return 2*fun(n+1);
+}
+console.log (fun(2))
+
+</script>
+
+<body>
+</html>
+ 
+// observed output 16
