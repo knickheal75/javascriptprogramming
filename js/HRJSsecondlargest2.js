10ee4c725f9218a726170c2b83650e4a13735ed9
knickheal75 <blitzkrieg0705@gmail.com>
2019-03-04 16:54:19 +0530

    alternative logic

---
+++ js/HRJSsecondlargest2.js
@@ -0,0 +1,36 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+    
+    main();    
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/**
+*   Return the second largest number in the array.
+*   @param {Number[]} nums - An array of numbers.
+*   @return {Number} The second largest number in the array.
+**/
+function getSecondLargest(nums) {
+    var aa = new Set(nums);
+    var arr = Array.from(aa);
+    arr.sort(function (a, b) { return a - b });
+    return (arr[arr.length-2]);
+}
+
