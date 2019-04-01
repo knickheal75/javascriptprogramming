581159839d9b0d11aa3c8eef8bee150fd4a2249b
knickheal75 <blitzkrieg0705@gmail.com>
2019-04-01 20:09:09 +0530

    adding tail/remove head,tail DLL

--- js/linkedlist.js
+++ js/linkedlist.js
@@ -12,6 +12,8 @@ function Node(prev, value, next) {
 
 }
 
+//ADDING TO HEAD
+
 LinkedList.prototype.addToHead = function(value) {
 
     let newNode = new Node(null, value, this.head);
@@ -26,4 +28,65 @@ LinkedList.prototype.addToHead = function(value) {
     this.head = newNode;
 
 };
+
+// ADDING TO TAIL 
+
+LinkedList.prototype.addToTail = function(value) {
+
+let newNode = new Node(this.tail, value, null);
+
+if(this.tail){
+    this.tail.next = newNode;
+}
+else{
+    this.head = newNode;
+}
+
+this.tail = newNode;
+
+};
+
+// REMOVING HEAD NODE
+
+LinkedList.prototype.removeHead = function(){
+
+    if(!this.head){
+        return null;
+    }
+
+    let val = this.head.value;
+
+    this.head = this.head.next;
+
+    if(this.head){
+        this.head.prev = null;
+    }
+    else {
+        this.tail = null;
+    }
+
+    return val;
+
+};
+
+//REMOVING TAIL NODE
+
+LinkedList.prototype.removeTail=function(){
+
+    if(!this.tail){
+        return null;
+    }
+
+    let val=this.tail.value;
+
+    this.tail=this.tail.prev; 
+
+    if(this.tail){
+        this.tail.next=null;
+    }
+    return val;
+
+};
+
+var ll1 = new LinkedList();
 
